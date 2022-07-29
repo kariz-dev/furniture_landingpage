742c0e32e5415391393ac79acac5c10a46659edc
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2022-07-30 02:56:05 +0700

    create navbar comp

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { NavbarComp } from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavbarComp></NavbarComp>
+    </div>
+  );
+}
+
+export default App;
